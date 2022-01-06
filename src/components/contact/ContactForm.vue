57957e0a9a7f74e6c810a37fff02807670711827
dan <danielalmenar@gmail.com>
2022-01-06 07:56:47 -0300

    added middleware and composables functionality for admin sign in and creation of documents into the database

---
+++ src/components/contact/ContactForm.vue
@@ -0,0 +1,41 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import { Ref, ref } from 'vue'
+import { ContactFormInput, SaveDocumentStatus } from '@/customTypes/customTypes'
+import { useSetDoc } from '@/composables/useSetDoc/useSetDoc'
+export default {
+    setup(){
+        const name: Ref<string> = ref('')
+        const email: Ref<string> = ref('')
+        const message: Ref<string> = ref('')
+
+        const { doc, func } = useSetDoc()
+
+        const submit = (): void => {
+            const formInput: Ref<ContactFormInput> = ref({
+                name: name,
+                email: email,
+                message: message,
+                date: new Date(Date.now())
+            })
+
+            func(formInput)
+        }
+
+        return {
+            name,
+            email,
+            message,
+            submit,
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
