4feab50960cc75d4351d9738357071233871f7ba
dan <danielalmenar@gmail.com>
2022-01-19 20:50:50 -0300

    AdminCreateProject is fully functional. Created the CreatedProject component with basic template (needs work)

---
+++ src/components/admin/CreateProject.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="create-project-form">
+      <form @submit.prevent="createProject" method="post" autocomplete="true">
+          <!-- TODO: add necessary fields -->
+          <button type="submit">{{ btnTag }}</button>
+      </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { inject, computed, ComputedRef, Ref } from 'vue'
+import { Language } from '@/customTypes/customTypes'
+export default {
+    name: 'CreateProject',
+    setup(){
+        const createProject:() => void = () => {
+            console.log('create project')
+        }
+        const lang: Ref<Language> = inject('lang') as Ref<Language>
+        const btnTag: ComputedRef<string> = computed(() => {
+            const tag = lang.value === 'english' ? 'Create Project' : 'Crear Proyecto'
+            return tag
+        })
+        return {
+            lang,
+            createProject,
+            btnTag,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/admin/AdminCreateProject.vue
+++ src/views/admin/AdminCreateProject.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-      <h1>(Admin) Create Project page...</h1>
-  </div>
+    <div class="error" v-if="!user.user">
+      <ErrorPage :err="forbidden" />
+    </div>
+    <div class="create-project-form" v-else>
+      <CreateProject />
+    </div>
 </template>
 
-<script>
+<script lang="ts">
+import CreateProject from '@/components/admin/CreateProject.vue'
+import ErrorPage from '@/components/common/ErrorPage.vue'
+import { inject, Ref } from 'vue'
+import { getters } from '@/composables/store/store'
+import { errors } from '@/customTypes/Errors'
+import { FirebaseUser } from '@/customTypes/customTypes'
 export default {
-
+  setup(){
+    const user: Ref<FirebaseUser> = getters.user()
+    const forbidden: Error = errors.forbidden
+    return {
+      user,
+      forbidden
+    }
+  },
+  components: {
+    ErrorPage,
+    CreateProject,
+  }
 }
 </script>
 
